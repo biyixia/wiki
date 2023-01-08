cb7082b88027ae947f7e95811800d95e0fa06ce5
biyixia <1207074847@qq.com>
2023-01-08 17:27:17 +0800

    首次提交：测试http client接口

---
+++ src/main/java/com/gec/wiki/config/WikiApplication.java
@@ -0,0 +1,25 @@
+package com.gec.wiki.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+//如果不加这个注解就只会扫描该包所在位置及其子包，因为换了位置 扫描多个包@ComponentScan("com.gec","com.test")
+@ComponentScan("com.gec")
+@SpringBootApplication
+public class WikiApplication
+{
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+
+    public static void main( String[] args )
+    {
+        //SpringApplication.run(WikiApplication.class,args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+
+    }
+}
---
+++ src/main/java/com/gec/wiki/controller/HelloWorldController.java
@@ -0,0 +1,31 @@
+package com.gec.wiki.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author dbc
+ * @create 2023-01-08 15:18
+ */
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String helloWorld(){
+        return "Hello World !";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World!"+name;
+    }
+
+    //测试从配置文件中获取属性值
+    @Value("${test.hello:test}")
+    private String testHello;
+
+    @GetMapping("/testHello")
+    public String testHello(){
+        return "Hello World!"+testHello;
+    }
+}
