e3baef8a77272e42f51c8eacd47dfe3737e23433
biyixia <1207074847@qq.com>
2023-01-10 10:58:26 +0800

    前端文件创建

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout>
+    <a-layout-footer style="text-align: center">
+      wiki电子书网站
+    </a-layout-footer>
+  </a-layout>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name:'the-footer',
+});
+</script>
